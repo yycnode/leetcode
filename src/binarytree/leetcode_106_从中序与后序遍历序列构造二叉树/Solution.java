35bef2425357d4e5deb59ba8f03c452fcc2009a5
杨跃承 <yyc2849739761@163.com>
2023-10-04 17:00:44 +0800

    leetcode_106_从中序与后序遍历序列构造二叉树 递归

---
+++ src/binarytree/leetcode_106_从中序与后序遍历序列构造二叉树/Solution.java
@@ -0,0 +1,27 @@
+package binarytree.leetcode_106_从中序与后序遍历序列构造二叉树;
+
+import binarytree.TreeNode;
+
+import java.util.Arrays;
+
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length == 0){
+            return null;
+        }
+        int rootValue = postorder[postorder.length - 1];
+        TreeNode root = new TreeNode(rootValue);
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootValue){
+                int[] inLeft = Arrays.copyOfRange(inorder, 0, i);
+                int[] inRight = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+                int[] postLeft = Arrays.copyOfRange(postorder, 0, i);
+                int[] postRight = Arrays.copyOfRange(postorder, i, postorder.length - 1);
+                root.left = buildTree(inLeft,postLeft);
+                root.right = buildTree(inRight,postRight);
+                break;
+            }
+        }
+        return root;
+    }
+}
