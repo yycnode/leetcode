e64e32718a5f61f168aa7545a45b96b74ad97246
杨跃承 <yyc2849739761@163.com>
2023-10-04 21:04:37 +0800

    二叉搜索树 找前任后任

--- src/binarysearchtree/BSTTree1.java
+++ src/binarysearchtree/BSTTree1.java
@@ -58,15 +58,19 @@ public class BSTTree1 {
      * @return 关键字对应的值
      */
     public Object min() {
-        if (root == null) {
+        return min(root);
+        /*return doMin(root); // 递归 */
+    }
+
+    public Object min(BSTNode node) {
+        if (node == null) {
             return null;
         }
-        BSTNode p = root;
-        while (p.left != null){
+        BSTNode p = node;
+        while (p.left != null) {
             p = p.left;
         }
         return p.value;
-        /*return doMin(root); // 递归 */
     }
 
     public Object doMin(BSTNode node) {
@@ -85,11 +89,15 @@ public class BSTTree1 {
      * @return 关键字对应的值
      */
     public Object max() {
-        if (root == null) {
+        return max(root);
+    }
+
+    private Object max(BSTNode node) {
+        if (node == null) {
             return null;
         }
-        BSTNode p = root;
-        while (p.right != null){
+        BSTNode p = node;
+        while (p.right != null) {
             p = p.right;
         }
         return p.value;
@@ -119,4 +127,70 @@ public class BSTTree1 {
         return node;
     }
 
+    /**
+     * <h3>查找关键字的前任值</h3>
+     *
+     * @param key 关键字
+     * @return 前任值
+     */
+    public Object predecessor(int key) {
+        BSTNode p = root;
+        BSTNode ancestorFromLeft = null; // 自左而来的祖先
+        while (p != null) {
+            if (key < p.key) {
+                p = p.left;
+            } else if (key > p.key) {
+                ancestorFromLeft = p;
+                p = p.right;
+            } else {
+                break;
+            }
+        }
+        // 没找到节点
+        if (p == null) {
+            return null;
+        }
+        // 找到节点
+        // 情况1：节点有左子树，此时前任就是左子树的最大值
+        if (p.left != null) {
+            return max(p.left);
+        }
+        // 情况2：节点没有左子树，若离它最近的、自左而来的祖先就是前任
+        return ancestorFromLeft != null ?
+                ancestorFromLeft.value : null;
+
+    }
+
+    /**
+     * <h3>查找关键字的后任值</h3>
+     *
+     * @param key 关键字
+     * @return 后任值
+     */
+    public Object successor(int key) {
+        BSTNode p = root;
+        BSTNode ancestorFromRight = null;
+        while (p != null) {
+            if (key < p.key) {
+                ancestorFromRight = p;
+                p = p.left;
+            } else if (p.key < key) {
+                p = p.right;
+            } else {
+                break;
+            }
+        }
+        // 没找到节点
+        if (p == null) {
+            return null;
+        }
+        // 找到节点
+        // 情况1：节点有右子树，此时后任就是右子树的最小值
+        if (p.right != null) {
+            return min(p.right);
+        }
+        // 情况2：节点没有右子树，若离它最近的、自右而来的祖先就是后任
+        return ancestorFromRight != null ?
+                ancestorFromRight.value : null;
+    }
 }
