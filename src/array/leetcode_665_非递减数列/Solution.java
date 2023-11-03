ce6852209c109df706cec5b74ec2959edca1ccdc
yangyueca <yyc2849739761@163.com>
2023-11-03 23:09:23 +0800

    leetcode_665_非递减数列

--- src/array/leetcode_665_非递减数列/Solution.java
+++ src/array/leetcode_665_非递减数列/Solution.java
@@ -13,19 +13,29 @@ public class Solution {
     // 4 2 3
     public static boolean checkPossibility(int[] nums) {
         int n = nums.length;
-        if (n <= 2){
+        if (n <= 2) {
             return true;
         }
+        boolean flag = nums[0] <= nums[1]; // 标识是否还能修改
+        // 遍历时，每次需要看连续的三个元素
         for (int i = 1; i < n - 1; i++) {
-            if ((nums[i - 1] > nums[i] && nums[i] > nums[i + 1]) || (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])){
-               return false;
+            if (nums[i] > nums[i + 1]) { // 出现递减
+                if (flag) {  // 如果还有修改机会，进行修改
+                    if (nums[i + 1] >= nums[i - 1]) {// 修改方案1
+                        nums[i] = nums[i + 1];
+                    } else {  // 修改方案2
+                        nums[i + 1] = nums[i];
+                    }
+                    flag = false;    // 用掉唯一的修改机会
+                } else        // 没有修改机会，直接结束
+                    return false;
             }
         }
         return true;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3,4,2,3};
+        int[] nums = {3, 4, 2, 3};
         System.out.println(checkPossibility(nums));
     }
 }
