0e81fa37b470a916393bdae6dac6848fb29dddd4
yangyueca <yyc2849739761@163.com>
2023-11-04 15:48:25 +0800

    Move Code to RandomArray.java

--- src/random/RandtoRand.java
+++ src/random/RandtoRand.java
@@ -63,38 +63,6 @@ public class RandtoRand {
         return ans;
     }
 
-    /**
-     * 返回随机数组
-     * @param maxLen
-     * @param maxValue
-     * @return
-     */
-    public static int[] lenRandomValueRandom(int maxLen, int maxValue){
-        int len = (int) (Math.random() * maxLen);
-        int[] ans = new int[len];
-        for (int i = 0; i < len; i++){
-            ans[i] = (int)(Math.random() * maxValue);
-        }
-        return ans;
-    }
-
-    public static int[] copyArray(int[] arr){
-        int[] ans = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            ans[i] = arr[i];
-        }
-        return ans;
-    }
-
-    public static boolean equalValues(int[] arr1, int[] arr2){
-        for (int i = 0; i < arr1.length; i++){
-            if (arr1[i] != arr2[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
         int count = 0;
         int testTimes = 10000;
